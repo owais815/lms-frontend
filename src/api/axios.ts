0bbf485609f71a7be201372263418881c72fb8c4
owais815 <sultanowaismehmood@gmail.com>
2026-02-19 02:35:07 +0500

    Update API URLs to point to the new server addresses

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -4,13 +4,16 @@ import axios from 'axios';
 // export const SOCKET_URL = "http://localhost:8080";
 
 //  const BASE_URL = "https://45.159.223.118:8443";
-export const MEETING_URL = "https://lmsapi.duckdns.org/join";
- const BASE_URL = "https://lmsapi.duckdns.org/api";
-export const SOCKET_URL = "https://lmsapi.duckdns.org";
+// export const MEETING_URL = "https://lmsapi.duckdns.org/join";
+//  const BASE_URL = "https://lmsapi.duckdns.org/api";
+// export const SOCKET_URL = "https://lmsapi.duckdns.org";
 
+const BASE_URL = 'http://62.171.177.9:8080/api';
+export const SOCKET_URL = 'http://62.171.177.9:8080';
+export const MEETING_URL = 'http://62.171.177.9:8080/join';
 
 const instance = axios.create({
-  baseURL: BASE_URL, 
+  baseURL: BASE_URL,
   timeout: 5000,
   headers: {
     'Content-Type': 'application/json',
